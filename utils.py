42e05b9201b794382a587230f02c224d257d46e3
javmarina <javmarina@uma.es>
2021-09-02 19:38:06 +0200

    Added Averager utility class

--- utils.py
+++ utils.py
@@ -6,6 +6,19 @@ from urllib import request
 import requests
 
 
+class Averager:
+    def __init__(self):
+        self._count = 0
+        self._avg = 0
+
+    def add_value(self, value):
+        self._avg = ((self._count * self._avg) + value) / (self._count + 1)
+        self._count += 1
+
+    def get_average(self):
+        return self._avg
+
+
 @unique
 class RobotModel(Enum):
     GIRONA_500_1 = "Girona 500 1"
